fa4f9c1724394301126d94eca581fce7553a5e18
Mtiass <MDLSproff@gmail.com>
2024-01-23 11:44:41 -0800

    Fixes

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(0,10):
+for i in range(0, 10):
     for c in range(i + 1, 10):
         if i != c:
             print("{}{}".format(i, c), end=", " if i != 8 or c != 9 else '\n')
--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def islower(c):
-    for i in range(97, 123):
-        if chr(i) == c:
-            return True
-        else:
-            continue
+    if c != None:
+        for i in range(97, 123):
+            if chr(i) == c:
+                return True
+            else:
+                continue
+            return False
     return False
