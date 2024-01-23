7ef4c094bbbb9719e87bdd2a4a3e23b33debff7c
Mtiass <MDLSproff@gmail.com>
2024-01-23 11:55:06 -0800

    Fixes

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def islower(c):
-    if c != None:
-        for i in range(97, 123):
-            if chr(i) == c:
-                return True
-            else:
-                continue
-            return False
-    else:
-        print(Traceback (most recent call last):)
+    if c is None:
+        raise TypeError("Traceback (most recent call last):")
+    for i in range(97, 123):
+        if chr(i) == c:
+            return True
+        else:
+            continue
+    return False
