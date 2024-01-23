de4971e96b41e1e0568f9e07af0448a2f47517d7
Mtiass <MDLSproff@gmail.com>
2024-01-23 12:22:50 -0800

    Fixes

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if c == '':
-        raise TypeError("Traceback (most recent call last):")
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
     else:
-        for i in range(97, 123):
-            if chr(i) == c:
-                return True
-            else:
-                continue
         return False
