c9c5af88b5382c8945a11bf0a91fb371c427ee89
Mtiass <MDLSproff@gmail.com>
2024-01-25 12:46:30 -0800

    Fix task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for fila in matrix:
         for i in range(0, len(fila)):
-            print("{}".format(fila[i]), end=' ' if i < (len(fila) - 1) else '')
+            print("{:d}".format(fila[i]), end=' ' if i < (len(fila) - 1) else '')
         print()
