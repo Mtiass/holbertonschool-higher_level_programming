5d4309ba9522b3229f9b394444f6cbe2c796cc30
Mtiass <MDLSproff@gmail.com>
2024-01-25 13:00:20 -0800

    task 7fix

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_c = [0, 0]
-    for i in range(len(tuple_a)):
-        tuple_c[i] += tuple_a[i]
-    for i in range(len(tuple_b)):
-        tuple_c[i] += tuple_b[i]
-    return tuple(tuple_c)
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    return (a[0] + b[0], a[1] + b[1])
