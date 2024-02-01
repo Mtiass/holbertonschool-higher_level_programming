00cce0f933f7ae74b518a7a9e4a63e0fe64e183a
Mtiass <MDLSproff@gmail.com>
2024-02-01 12:00:07 -0800

    5-square.py

---
+++ python-classes/5-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+This module defines Square as a class.
+"""
+
+
+class Square:
+    """
+    This is a class where a square is defined.
+
+    Attributes:
+        size: is a private instance attribute.
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """
+        This is a public instance method to calculate the area of a square.
+        """
+
+        sqarea = self.__size ** 2
+        return (sqarea)
+
+    def my_print(self):
+        """
+        This is a public instance method to print in stdout a square using #.
+        """
+
+        if self.__size == 0:
+            print()
+        for row in range(self.__size):
+            for column in range(self.__size):
+                print("#", end="")
+            print()
