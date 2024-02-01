d9d5e8a9972dac4798ace1ebcf2bbc31146ce417
Mtiass <MDLSproff@gmail.com>
2024-02-01 10:44:52 -0800

    1-square

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module defines Square as a class.
+"""
+
+
+class Square:
+    """
+    This is a class where a square is defined.
+
+    Attributes:
+        size: is a private instance attribute.
+    """
+    def __init__(self, size):
+        self.__size = size
