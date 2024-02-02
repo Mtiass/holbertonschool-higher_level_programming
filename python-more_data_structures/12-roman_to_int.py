80875cf38f24a0f99aaf26e37ed6dec1128f3c9f
Mtiass <MDLSproff@gmail.com>
2024-02-02 07:20:37 -0800

    12-roman_to_int

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,11 +3,15 @@ def roman_to_int(roman_string):
 
     if len(roman_string) == 0 or roman_string is None:
         return (0)
+    if not isinstance(roman_string, str):
+        return (0)
 
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
 
     for i in range(len(roman_string)):
+        if roman_string[i] not in roman:
+            return (0)
 
         current = roman[roman_string[i]]
 
@@ -22,4 +26,4 @@ def roman_to_int(roman_string):
             else:
                 result += current
 
-    return result
+    return (result)
