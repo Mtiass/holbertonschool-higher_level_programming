0e76a1e9eba37769c7321da6d18689a091f424e0
Mtiass <MDLSproff@gmail.com>
2024-02-05 10:27:21 -0800

    5-rectangle

---
+++ python-more_classes/5-rectangle.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python3
+"""
+This module defines a class as Rectangle.
+"""
+
+
+class Rectangle:
+    """
+    This is an empty class that defines a rectangle.
+
+    Attributes:
+        width: is a private instance attribute that defines the
+        Rectangle's width.
+        height: is a private instance attribute which defines the
+        Rectangle's height.
+    """
+
+    def __init__(self, width=0, height=0):
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        return (self.__width)
+
+    @property
+    def height(self):
+        return (self.__height)
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """
+        This is a public method that returns the rectangle's area.
+        """
+
+        area = self.__height * self.__width
+        return (area)
+
+    def perimeter(self):
+        """
+        This is a public method that returns the rectangle's perimeter.
+        """
+
+        if self.__height == 0 or self.__width == 0:
+            perimeter = 0
+            return (perimeter)
+
+        perimeter = 2 * (self.__height + self.__width)
+        return (perimeter)
+
+    def __str__(self):
+        """
+        This is a method that returns a human-readable string,
+        that is a representation of the object.
+        It generates output for end user.
+        """
+
+        string = ""
+        if self.__width == 0 or self.__height == 0:
+            return (string)
+
+        for row in range(self.__height):
+            for column in range(self.__width):
+                string += "#"
+            if row < self.__height - 1:
+                string += '\n'
+        return (string)
+
+    def __repr__(self):
+        """
+        This is a method that returns a string representation of the object.
+        It generates output for developer.
+        """
+
+        return ("Rectangle({}, {})".format(self.__width, self.__height))
+
+    def __del__(self):
+        """
+        This is a method to delete an instance of Reactangle.
+        """
+
+        print("Bye rectangle...")
