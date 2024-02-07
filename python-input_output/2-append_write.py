5f410c043169ac6643067a571698421241657d97
Mtiass <MDLSproff@gmail.com>
2024-02-07 13:42:56 -0800

    2-append_write

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module defines a function called append_write.
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    This function appends a string at the end of a text file
+    and returns the number of characters added.
+    """
+
+    with open(filename, "a", encdoing="utf-8") as file:
+        return (filename.append(text))
