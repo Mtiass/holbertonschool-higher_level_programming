b6b7af2dcc0b16222bb7ce675c5804747346296d
Mtiass <MDLSproff@gmail.com>
2024-02-07 13:26:34 -0800

    1-write_file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines a function write_file.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    This function writes a string to a text file and returns
+    the number of characters written.
+    """
+
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(text)
+
+        return(len(text))
