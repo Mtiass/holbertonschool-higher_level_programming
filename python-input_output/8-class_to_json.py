68fffc8ba1d61dee22ec4c482ae1b72a24e28c0e
Mtiass <MDLSproff@gmail.com>
2024-02-07 15:55:37 -0800

    8-class_to_json

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module defines class_to_json function.
+"""
+
+
+def class_to_json(obj):
+    """
+    This function returns the dictionary description with simple data
+    structure for JSON serialization of an object.
+    """
+
+    return (dict(obj))
