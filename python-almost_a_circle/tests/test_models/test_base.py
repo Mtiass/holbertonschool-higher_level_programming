83a32e2e44ec44a0c7b20b60a89fc402e952e57c
Mtiass <MDLSproff@gmail.com>
2024-02-15 14:45:40 -0800

    Tests

---
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Unittest.
+"""
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+
+class testbase(unittest.TestCase):
+    """
+    This is a class to test the Base class.
+    """
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This is the unittest for the Square class.
+"""
+import unittest
+from models.square import Square
+
+
+class testSquare(unittest.TestCase):
+    """
+    This is a class to test the Square class
+    """
+
+
+if __name__ == '__main__':
+    unittest.main()
