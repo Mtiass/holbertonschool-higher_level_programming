9296532616fd9f47ab994d9f729bd77da9a877ad
Mtiass <MDLSproff@gmail.com>
2024-03-11 17:00:26 -0300

    13th fixes

--- SQL_introduction/13-change_class.sql
+++ SQL_introduction/13-change_class.sql
@@ -1,3 +1,3 @@
 -- script that removes all records with a score <= 5
 -- in second_table ofhbtn_0c_0 in your MySQL server.
-DELETE FROM second_table WHERE `score` <= 10;
+DELETE FROM second_table WHERE `score` <= 5;
