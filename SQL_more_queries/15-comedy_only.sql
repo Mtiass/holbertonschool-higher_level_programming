a4f13f01333b2031da73694f5ee538b8ec9b4b81
Mtiass <MDLSproff@gmail.com>
2024-03-12 19:50:38 -0300

    15 fix

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,5 @@
 -- Script that lists all Comedy shows in the database hbtn_0d_tvshows.
 SELECT tv_shows.title FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-INNER JOIN tv_genres ON tv_genres.id = tv_shows_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = 'Comedy' ORDER BY tv_shows.title ASC;
