776470d9851ce514f1240fad24c73190a480f0ca
Mtiass <MDLSproff@gmail.com>
2024-03-12 19:28:09 -0300

    13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,5 +2,5 @@
 -- and displays the number of shows linked to each.
 SELECT tv_genres.name AS 'genre', COUNT(*) AS `number_of_shows`
 FROM tv_genres INNER JOIN tv_shows_genres
-ON tv_genres.id = tv_shows_genres.genre_id GROUP BY tv_genres.name 
+ON tv_genres.id = tv_shows_genres.genre_id GROUP BY tv_genres.id 
 ORDER BY number_of_shows DESC;
