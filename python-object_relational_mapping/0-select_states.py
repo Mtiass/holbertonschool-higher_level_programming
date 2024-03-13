729880bafb4ff297c4ec5efc893f2ccc58480129
Mtiass <MDLSproff@gmail.com>
2024-03-13 09:28:51 -0700

    task 0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Script that lists all states from the database hbtn_0e_0_usa.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    con = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        host='localhost',
+        port=3306
+    )
