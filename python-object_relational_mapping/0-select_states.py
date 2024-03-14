d367de3397ad6ff9c9e47eb6569b006e377b6a3e
Mtiass <MDLSproff@gmail.com>
2024-03-14 12:49:30 -0300

    crying

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,17 +5,11 @@ import MySQLdb
 
 
 if __name__ == '__main__':
-
-    if len(argv) < 4:
-        print("Usage: {} username password database_name".format(argv[0]))
-        exit(1)
     con = MySQLdb.connect(
         port=3306, user=argv[1], passwd=argv[2],
         db=argv[3], host="localhost")
     cursor = con.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    ns = cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
     statesr = cursor.fetchall()
     for state in statesr:
         print(state)
-    cursor.close()
-    con.close()
