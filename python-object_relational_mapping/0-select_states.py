1de9d27163ef0e20de53a8ce3aeb458a8ad2986e
Mtiass <MDLSproff@gmail.com>
2024-03-14 12:27:02 -0300

    t0 fix

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,12 +5,13 @@ if __name__ == '__main__':
     import sys
     import MySQLdb
 
-    con = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
-    cursor = con.cursor()
-    cursor.execute("SELECT * FROM states")
-    statesr = cursor.fetchall()
-    for state in statesr:
-        print(state)
-    cursor.close()
-    con.close()
+    try:
+        con = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                            passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
+        cursor = con.cursor()
+        statesr = cursor.fetchall()
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        for state in statesr:
+            print(state)
+    except IndexError:
+        print("IndexError")
