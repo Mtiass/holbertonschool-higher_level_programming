15ace6ccf2b37fc2b05db6294ab8547c8018e806
Mtiass <MDLSproff@gmail.com>
2024-03-14 11:38:41 -0300

    Solution?

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,11 +2,12 @@
 """
 Script that lists all states from the database hbtn_0e_0_usa.
 """
-import MySQLdb
-import sys
 
 
 if __name__ == '__main__':
+    import MySQLdb
+    import sys
+    
     con = MySQLdb.connect(
         user=sys.argv[1],
         passwd=sys.argv[2],
