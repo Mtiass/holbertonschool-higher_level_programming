0d6608baa3be59beb0099d3e6596e8ad0a4d47cf
Mtiass <MDLSproff@gmail.com>
2024-03-14 13:16:35 -0300

    rotten

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -9,10 +9,10 @@ if __name__ == '__main__':
         port=3306, user=argv[1], passwd=argv[2],
         db=argv[3], host="localhost")
     cursor = con.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' "
+                   "ORDER BY id ASC")
     statesr = cursor.fetchall()
     for state in statesr:
-        if state[1][0] == "N":
-            print(state)
+        print(state)
     cursor.close()
     con.close()
