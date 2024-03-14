72ccbcd60116e19a0856e2e124c2c8f374d095e0
Mtiass <MDLSproff@gmail.com>
2024-03-14 12:03:57 -0300

    Task 0 fix

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""
-Script that lists all states from the database hbtn_0e_0_usa.
-"""
+# Script that lists all states from the database hbtn_0e_0_usa.
 import sys
 import MySQLdb
 
-def main():
+
+if __name__ == '__main__':
 
     con = MySQLdb.connect(
         user=sys.argv[1],
@@ -17,10 +16,6 @@ def main():
 
     cursor = con.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    states = cursor.fetchall()
-
-    for state in states:
-        print(state)
-
-if __name__ == '__main__':
-    main()
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
