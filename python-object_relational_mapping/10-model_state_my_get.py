cd4a27f24047fa009bb74613aca0640c5f0a1a74
Mtiass <MDLSproff@gmail.com>
2024-03-17 09:44:18 -0700

    10-model_state_my_get.py

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Script that prints the State object with the name passed as argument
+from the database hbtn_0e_6_usa.
+"""
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    cur = session.query(State).filter(State.name == argv[4]).first()
+    if not cursor:
+        print("Not found")
+        exit
+    else:
+        print(cur.id)
