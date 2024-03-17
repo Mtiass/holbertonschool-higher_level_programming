ec33ff4a99efad9e7306c923ddf3f24fcb48da9c
Mtiass <MDLSproff@gmail.com>
2024-03-17 09:36:35 -0700

    8-model_state_fetch_first.py

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).order_by(State.id).first()
+    state = session.query(State).order_by(State.id).first()
     if state is None:
         print("Nothing")
     else:
