7984ac8faedafc279a5186b5cc02b07d59dafd57
Mtiass <MDLSproff@gmail.com>
2024-03-17 07:49:09 -0700

    0-select_states fix

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """ Script that lists all states from the database hbtn_0e_0_usa."""
-from sys import argv
-import MySQLdb
 
 
 if __name__ == '__main__':
-    con = MySQLdb.connect(
-        port=3306, user=argv[1], passwd=argv[2],
-        db=argv[3], host="localhost")
-    cursor = con.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
-    statesr = cursor.fetchall()
-    for state in statesr:
-        print(state)
-    cursor.close()
-    con.close()
+    from sys import argv
+    import MySQLdb
+    try:
+        con = MySQLdb.connect(
+                port=3306, user=argv[1], passwd=argv[2],
+                db=argv[3], host="localhost")
+        cursor = con.cursor()
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        for state in cursor.fetchall():
+            print(state)
+        cursor.close()
+        con.close()
+    except Exception as N:
+        print("ERROR: {}".format(N))
