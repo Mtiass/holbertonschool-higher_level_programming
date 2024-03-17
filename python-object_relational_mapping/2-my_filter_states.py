87fadcb653002bdaef3da48261e785b1c8baab1f
Mtiass <MDLSproff@gmail.com>
2024-03-17 08:25:04 -0700

    2-my_filter_states

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -12,9 +12,9 @@ if __name__ == '__main__':
             port=3306, user=argv[1], passwd=argv[2],
             db=argv[3], host="localhost")
     cursor = con.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    for state in cursor.fetchall():
-        if state[1] == argv[4]:
+    cursor.execute("SELECT * FROM states WHERE name = '{}'\
+            ORDER BY id ASC".format(argv[4]))
+    for state in cursor.fetchall():    
             print(state)
     cursor.close()
     con.close()
