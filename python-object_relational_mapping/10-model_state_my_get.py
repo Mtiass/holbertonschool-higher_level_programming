11a4e632faea398c9027361e37bdbb809f3e0d23
Mtiass <MDLSproff@gmail.com>
2024-03-17 09:48:19 -0700

    10-model_state_my_get.py

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -12,10 +12,11 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == '__main__':
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
     session = Session()
     cur = session.query(State).filter(State.name == argv[4]).first()
-    if not cursor:
+    if not cur:
         print("Not found")
         exit
     else:
