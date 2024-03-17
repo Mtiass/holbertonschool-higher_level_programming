f4952cfe0800fc9bb8b018fddd4519e600faccba
Mtiass <MDLSproff@gmail.com>
2024-03-17 08:08:46 -0700

    1-filter_states fix

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-""" Script that lists all states starting with N from hbtn_0e_0_usa."""
-from sys import argv
-import MySQLdb
+"""Script that lists all states starting with N from hbtn_0e_0_usa."""
 
 
 if __name__ == '__main__':
-    con = MySQLdb.connect(
-        port=3306, user=argv[1], passwd=argv[2],
-        db=argv[3], host="localhost")
-    cursor = con.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
-    statesr = cursor.fetchall()
-    for state in statesr:
-        if state[1][0] == "N":
-            print(state)
-    cursor.close()
-    con.close()
-    
+    from sys import argv
+    import MySQLdb
+    try:
+        con = MySQLdb.connect(
+                port=3306, user=argv[1], passwd=argv[2],
+                db=argv[3], host="localhost")
+        cursor = con.cursor()
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        for state in cursor.fetchall():
+            if state[1][0] == "N":
+                print(state)
+        cursor.close()
+        con.close()
+    except Exception as N:
+        print("ERROR: {}".format(N))    
