28628c3c1fa41fa22bf00a158a2207967eda8f33
Mtiass <MDLSproff@gmail.com>
2024-05-07 16:54:54 -0300

    Readme and task 0

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,4 @@
+// JavaScript script that updates the text color of the header element to red (#FF0000)
+const headerelement = document.querySelector('h1');
+headerelement.style.color = "#FF0000";
+ 
