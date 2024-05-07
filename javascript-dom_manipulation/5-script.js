0770f45888261cf363171db3828eb62990a9b55f
Mtiass <MDLSproff@gmail.com>
2024-05-07 17:08:43 -0300

    task 5

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,5 @@
+// Script that updates the text of the header element to New Header.
+function updateHeaderText() {
+    var headerElement = document.getElementById("header");
+    headerElement.innerText = "New Header!!!";
+}
